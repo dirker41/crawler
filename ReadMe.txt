80b0928425c0f3f31f598bdab82ccde25952d1a1
li <dirker41@hotmail.com>
2017-04-12 14:04:22 +0800

    add ignore

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,13 +1,13 @@
 雙擊 run.bat 並輸入關鍵字
 會在 https://www.jkforum.net/home.php?mod=space&uid=430045&do=thread&view=me&from=space
 底下搜尋關鍵字
-並將結果輸出在 link.html上
+並將結果輸出在 關鍵字.html
 
 關鍵字用 空格 隔開 會做AND搜尋 
 EX: 中文 081316-229 
 會搜尋內容同時含有'中文'跟'081316-229'的文章
 
-沒有搜尋完一樣可以檢視link.html
+沒有搜尋完一樣可以檢視HTML
  
 環境:
 Python 3.6.1
--- crawler.py
+++ crawler.py
@@ -20,28 +20,33 @@ def searchUrl( url,searchStr ):
 def writehHtml( url ):
     print( url + '\n' )
     file = open(fileName, 'a')
-    file.write( '<a href=\"' + url + '\">' + url + '</a><BR>' )
+    file.write( '<a target=\"_blank\" href=\"' + url + '\">' + url + '</a><BR>' )
     file.close()
 
 fileName = 'link.html'
 fileCount = 0 
-
+"""
 while exists(fileName):
   print( fileName )
   fileCount += 1 
   fileName = fileName[:4] + str(fileCount) + fileName[ len(fileName)-5:len(fileName)]
-
+"""
 
 searchStr = input("Enter key word:")
 
-file = open(fileName, 'w')
-file.write( searchStr + '<BR>' )
+fileName = searchStr + '.html'
+
+file = open(fileName, 'wb' )
+file.write( searchStr.encode('utf-8')  )
+file.close() 
+file = open(fileName, 'a' )
+file.write( '<BR>'  )
 file.close() 
 
 
 
 
-for i in range(1,500):
+for i in range(1,100):
  JKFstr = 'https://www.jkforum.net/home.php?mod=space&uid=430045&do=thread&view=me&order=dateline&from=space&page='
 
  res = requests.get(JKFstr+str(i),headers={'Connection':'close'})
